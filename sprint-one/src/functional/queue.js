8325204b00133c6bbcd3d7c91d2dd7ab8cd447f3
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-07 16:01:48 -0600

    (feat) Add functional queue implementation in DS.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[last++] = value;
+    console.log(last);
   };
 
   someInstance.dequeue = function() {
+    var tempData = storage[first];
+    if (last - first > 0) {
+    delete storage[first];
+    first++;
+    }
+    return tempData;
   };
 
   someInstance.size = function() {
+    return last - first;
   };
 
   return someInstance;
