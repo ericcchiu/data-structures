11cc6438111822951d96aff0ecc1b393edad5a90
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-07 17:13:43 -0600

    (feat) Add queue and stack functional shared inheritance pattern.Broken

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Creating the base object to store the methods and properties 
+  var someInstance = {}; 
+  
+  // Define properties 
+  someInstance.storage = {}; 
+  someInstance.first = 0; 
+  someInstance.last = 0; 
+  
+  // Extending object to include methods (delegation);
+  _.extend(someInstance, queueMethods);
+
+
+  return someInstance; 
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  // Add to back 
+  this.storage[this.last++] = value; 
+}; 
+
+queueMethods.dequeue = function() {
+  // store removed data 
+  var tempData = this.storage[this.first];
+  // Check if queue is empty 
+  if (this.last - this.first > 0) { 
+    // Remove property from storage 
+    delete this.storage[this.first]; 
+    // Increment 
+    this.first++; 
+  }
+  // Return removed data 
+  return tempData; 
+}; 
+
+queueMethods.size = function() {
+  // Return queue length 
+  return this.last - this.first; 
+}; 
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,47 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.count = 0;
+  
+  // Extending someInstance object to include stackMethods
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  // Alternative 
+  // _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
+stackMethods.push = function(value) {
+  // Instantiate new property in stack
+  this.storage[this.count++] = value;
+};
+
+stackMethods.pop = function() {
+  // Check if stack is empty return 0 if true
+  if (this.count < 1) { 
+    return 0; 
+  } 
+  // Store property prior to removal from stack
+  var deletedData = this.storage[this.count]; 
+  // Remove property from stack
+  delete this.storage[this.count];
+  // Decrement count
+  this.count--; 
+  
+  // Return removed property
+  return deletedData;
+
+};
+
+stackMethods.size = function() {
+  // Return the size of stack
+  return this.count;
+};
+
 
 
