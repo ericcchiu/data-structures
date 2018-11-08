48b21d8727f1a724b01c1d6f573ac13202770e49
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-08 15:40:06 -0600

    (feat) Add methods to Tree data structure in functional shared inheritance pattern.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,51 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fixed me
+
+  // Extend the newTree object
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
+// Creating tree methods outside of Tree maker function
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // Push new node to child array
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // Set boolean tracker
+  var result = false;
+  // Check if parent node has target value
+  if (this.value === target) {
+    result = true;
+  }
+  // Use recursion to traverse tree
+  var traverse = function(currentNode) {
+    // Iterate over current children array
+    for (var i = 0; i < currentNode.length; i++) {
+      // Check if current node is target
+      if (currentNode[i].value === target) {
+        result = true;
+      }
+      // Else continue traversal
+      traverse(currentNode[i].children);
+    }
+    // Return result
+    return result;
+  };
+  // Begin traversal and return result
+  return traverse(this.children);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild: O(1) (Constant because you are adding the child to the current node. Like addTail in linked list)
+ * contains: O(n) (Linear because you have to access every node in the tree until target is found) 
  */
