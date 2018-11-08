78620bb268a0b8785a364525a56ea723e4ca879d
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-07 19:53:42 -0600

    (feat) Add stack and queue in pseudoclassical inheritance pattern.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create queue object by setting properties
+  this.storage = {};
+  this.first = 0;
+  this.last = 0;
+
 };
 
+Queue.prototype.enqueue = function(value) {
+  // Increment last and add value to queue
+  this.storage[this.last++] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  // Create copy of removed data
+  var tempData = this.storage[this.first];
+  
+  // Check if queue is not empty
+  if (this.last - this.first > 0) {
+    // Delete prop
+    delete this.storage[this.first];
+    // Increment first
+    this.first++;
+  }
+    
+  // Return removed data
+  return tempData;  
+};
+
+Queue.prototype.size = function() {
+  return this.last - this.first;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create stack storage structure
+  this.storage = {}; 
+  this.count = 0; 
 };
 
+Stack.prototype.push = function(value) { 
+  // Increment count
+  this.count++; 
+  // Add property to top of stack
+  this.storage[this.count] = value;
+};
+
+Stack.prototype.pop = function() { 
+  // Store soon deleted property 
+  var tempData = this.storage[this.count]; 
+
+  // Check if stack is empty 
+  if (this.count < 1) { 
+    return 0; 
+  } 
+  
+  // Remove property from top of stack
+  delete this.storage[this.count];
+  
+  // Decrement by 1
+  this.count--; 
+
+  // Return removed value
+  return tempData;
+};
+
+Stack.prototype.size = function() { 
+  return this.count;
+};
 
